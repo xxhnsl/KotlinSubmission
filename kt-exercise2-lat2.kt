2be25f898a4b11ca23c5def8d0c010c38756503c
Lizzam Sekar Nollo Henning <108021709+xxhnsl@users.noreply.github.com>
2023-06-10 21:45:55 +0700

    Add files via upload

---
+++ kt-exercise2-lat2.kt
@@ -0,0 +1,50 @@
+package id.infinitelearning.KotlinSubmission.exercise2
+
+
+    /** Latihan 2 Map
+    Buatlah variabel bertipe Map dengan kriteria:
+    Daftar nama bulan dalam setahun,
+    - Set Key nya dengan inisial nama bulan dan Value nya nama bulan, contoh
+    - key "Jan", value January
+    - key "Feb", value February
+    - dst...
+    Cetak semua nama bulan dengan fungsi forEach sehingga hasil output seperti:
+    - Jan -> January
+    - Feb -> February
+    - Dst...
+
+    Cetak nama bulan sekarang dan bulan lahir kamu dengan format string berikut:
+    - "It's {$monthNow} now, I was born in {$birthMonth}"
+     */
+    // Buat di bawah sini
+
+fun main() {
+    val monthMap = mapOf(
+        "Jan" to "January",
+        "Feb" to "February",
+        "Mar" to "March",
+        "Apr" to "April",
+        "May" to "May",
+        "Jun" to "June",
+        "Jul" to "July",
+        "Aug" to "August",
+        "Sep" to "September",
+        "Oct" to "October",
+        "Nov" to "November",
+        "Dec" to "December"
+    )
+
+    // Print all month names with forEach function
+    monthMap.forEach { (key, value) ->
+        println("$key -> $value")
+    }
+
+    // Print current month's name and birth month
+    val monthNow = "Jun" // Replace with the current month's key
+    val birthMonth = "Oct" // Replace with your birth month's key
+
+    val currentMonthName = monthMap[monthNow]
+    val birthMonthName = monthMap[birthMonth]
+
+    println("Sekarang adalah bulan $currentMonthName, aku lahir bulan $birthMonthName")
+}
